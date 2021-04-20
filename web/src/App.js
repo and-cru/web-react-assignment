492e8bb1abb316cebb468dd9a2422188105de858
pedreviljoen <pedre87@gmail.com>
2021-04-20 10:14:21 +0200

    Fixed CORS issue with API

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+const API_URL = 'http://localhost:8080'
 
 function App() {
+  const checker = async () => {
+    const health = await axios.get(`${API_URL}/health`, {
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    })
+
+    console.log('Healh: ', health)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={checker}>
+          <p>
+            Click me!
+          </p>
+        </button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +32,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to React
+          Hi Jonny
         </a>
       </header>
     </div>
