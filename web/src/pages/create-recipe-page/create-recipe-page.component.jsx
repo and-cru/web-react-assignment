b0efb57c6b968f84dc52c9db2f82748c6474eb2c
Tyrone Jensen <56504946+drtjensen@users.noreply.github.com>
2021-04-26 23:58:46 +0200

    added useState|handleSubmit|handleChange|fetch(POST) to create-recipe component

--- web/src/pages/create-recipe-page/create-recipe-page.component.jsx
+++ web/src/pages/create-recipe-page/create-recipe-page.component.jsx
@@ -1,11 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import FormInput from "../../components/form-input/form-input.component";
 
-// import styles
+import "./create-recipe-page.styles.scss";
 
-const CreateRecipePage = () => (
-  <div className="create-recipe-page">
-    <h1>Create Recipe Page</h1>
-  </div>
-);
+const CreateRecipe = () => {
+  const [data, setData] = useState({
+    title: "",
+    description: "",
+    bean_type: "",
+    brew_time: "",
+    brew_method: "",
+    taste_notes: "",
+    tags: "",
+  });
 
-export default CreateRecipePage;
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setData((prevData) => {
+      return {
+        ...prevData,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(`http://localhost:8080/brewers/2/recipes/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    const resJson = await response.json();
+    console.log(resJson);
+  };
+
+  return (
+    <div className="create-recipe">
+      <h1>Create Recipe</h1>
+      <form className="create-recipe-form" onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          name="title"
+          value={data.title}
+          onChange={handleChange}
+          label="Title"
+          required
+        />
+        <FormInput
+          type="text"
+          name="description"
+          value={data.description}
+          onChange={handleChange}
+          label="Description"
+          required
+        />
+        <FormInput
+          type="text"
+          name="bean_type"
+          value={data.bean_type}
+          onChange={handleChange}
+          label="Bean Type"
+          required
+        />
+        <FormInput
+          type="text"
+          name="brew_time"
+          value={data.brew_time}
+          onChange={handleChange}
+          label="Brew Time"
+          required
+        />
+        <FormInput
+          type="text"
+          name="brew_method"
+          value={data.brew_method}
+          onChange={handleChange}
+          label="Brew Method"
+          required
+        />
+        <FormInput
+          type="text"
+          name="taste_notes"
+          value={data.taste_notes}
+          onChange={handleChange}
+          label="Taste Notes"
+          required
+        />
+        <button type="submit">POST</button>
+      </form>
+    </div>
+  );
+};
+export default CreateRecipe;
