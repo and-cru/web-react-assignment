d0f0ab53000f03d3294c93d05a2f1b379cdfaba6
Tyrone Jensen <56504946+drtjensen@users.noreply.github.com>
2021-04-26 22:15:08 +0200

    Initial commit: installed packages, added assets|components|pages folders, added .jsx|.scss to pages subfolders

--- web/src/App.js
+++ web/src/App.js
@@ -1,27 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-const API_URL = 'http://localhost:8080'
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+const API_URL = "http://localhost:8080";
 
 function App() {
   const checker = async () => {
     const health = await axios.get(`${API_URL}/health`, {
       headers: {
-        "Access-Control-Allow-Origin": "*"
-      }
-    })
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
 
-    console.log('Healh: ', health)
-  }
+    console.log("Healh: ", health);
+  };
+
+  const CorsChecker = async () => {
+    const response = await fetch(
+      "http://localhost:8080/brewers/?skip=0&limit=100"
+    );
+    const resJson = await response.json();
+
+    console.log(resJson[0]["recipes"][0].description);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <button onClick={checker}>
-          <p>
-            Click me!
-          </p>
+          <p>Click me!</p>
+        </button>
+        <button onClick={CorsChecker}>
+          <p>View Brewers</p>
         </button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
