d6a094105eb084738bb97e960f2d1c131d36599a
Tyrone Jensen <56504946+drtjensen@users.noreply.github.com>
2021-04-27 01:22:18 +0200

    Added currentUser useContext to header component to allow conditional rendering

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,37 @@
+import React, { useContext } from "react";
 import { Route, Switch } from "react-router-dom";
+import {
+  CurrentUserContext, // REMOVE
+  CurrentUserProvider,
+} from "./components/contexts/users/current-user.context";
 // import axios from "axios";
 
 import "./App.css";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/home-page/home-page.component";
-import CreateBrewerPage from "./pages/create-brewer-page/create-brewer-page.component";
-import CreateRecipePage from "./pages/create-recipe-page/create-recipe-page.component";
 import ProfilePage from "./pages/profile-page/profile-page.component";
 import BrewersRecipesPage from "./pages/brewers-recipes-page/brewers-recipes-page.component";
-import Header from "./components/header/header.component";
+import CreateBrewerPage from "./pages/create-brewer-page/create-brewer-page.component";
+import CreateRecipePage from "./pages/create-recipe-page/create-recipe-page.component";
 
 function App() {
+  //TO BE REMOVED: DEVELOPMENT ONLY! track currentUser state
+  const currentUser = useContext(CurrentUserContext);
+  console.log(currentUser);
+  // -----------------------------------------
   return (
     <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/profile" component={ProfilePage} />
-        <Route path="/brewersrecipes" component={BrewersRecipesPage} />
-        <Route path="/createbrewer" component={CreateBrewerPage} />
-        <Route path="/createrecipe" component={CreateRecipePage} />
-      </Switch>
+      <CurrentUserProvider>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/brewersrecipes" component={BrewersRecipesPage} />
+          <Route path="/createbrewer" component={CreateBrewerPage} />
+          <Route path="/createrecipe" component={CreateRecipePage} />
+        </Switch>
+      </CurrentUserProvider>
     </div>
   );
 }
--- web/src/components/header/header.component.jsx
+++ web/src/components/header/header.component.jsx
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { CurrentUserContext } from "../contexts/users/current-user.context";
 
 import "./header.styles.scss";
 
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
 const Header = () => {
+  const currentUser = useContext(CurrentUserContext);
+  console.log(currentUser); //REMOVE
+
   return (
     <div className="header">
       <Link className="logo-container" to="/">
         <Logo className="logo" />
       </Link>
       <div className="options">
-        <Link className="option" to="/profile">
-          My Profile
-        </Link>
+        {currentUser.id && (
+          <Link className="option" to="/profile">
+            My Profile
+          </Link>
+        )}
         <Link className="option" to="/brewersrecipes">
           Recipes
         </Link>
-        <Link className="option" to="/createrecipe">
-          Create Recipe
-        </Link>
-        <Link className="option" to="/createbrewer">
-          Create Brewer
-        </Link>
+        {!currentUser.id ? (
+          <Link className="option" to="/createrecipe">
+            Create Recipe
+          </Link>
+        ) : (
+          <Link className="option" to="/createbrewer">
+            Create Brewer
+          </Link>
+        )}
       </div>
     </div>
   );
